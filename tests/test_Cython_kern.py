75a6fd6f5d5e147b160719bbb1e04b929cae00d0
Michael D. Schneider <mdschneider@me.com>
2015-04-24 20:25:23 -0700

    Added a simple test comparing the python and cython interfaces to the ExpSquaredKernel.
    - Mostly for @mdschneider to learn the testing setup.

--- tests/test_Cython_kern.py
+++ tests/test_Cython_kern.py
@@ -4,7 +4,8 @@ from george.kernels import (KappaKappaExpSquaredKernel,
                             KappaGamma2ExpSquaredKernel,
                             Gamma1Gamma1ExpSquaredKernel,
                             Gamma2Gamma2ExpSquaredKernel,
-                            Gamma1Gamma2ExpSquaredKernel)
+                            Gamma1Gamma2ExpSquaredKernel,
+                            ExpSquaredKernel)
 import george
 import numpy as np
 import sys
@@ -15,7 +16,7 @@ from kern_deriv import KappaGamma2ExpSquaredKernel as KG2ker
 from kern_deriv import Gamma1Gamma1ExpSquaredKernel as G1G1ker
 from kern_deriv import Gamma2Gamma2ExpSquaredKernel as G2G2ker
 from kern_deriv import Gamma1Gamma2ExpSquaredKernel as G1G2ker
-from plot_kern_deriv import plotDerivCov
+from plot_kern_deriv import plotDerivCov, plotExpSqCov
 
 # --------------------- starting to test Cython Kernel values --------
 
@@ -47,8 +48,26 @@ def test_Cython_kappakappa_10_coords_vary_beta():
                  for beta in betas]
     cythonCov = [cythonGP.get_matrix(coords)
                  for cythonGP in cythonGPs]
+    # print("cythonCov:", cythonCov)
 
     pythonCov = [plotDerivCov(KKker, coords, beta)
+                 for beta in betas]
+    # print("pythonCov:", pythonCov[0])
+
+    for i in range(len(betas)):
+        assert np.array_equal(cythonCov[i], pythonCov[i])
+
+
+def test_Cython_sqExp_10_coords_vary_beta():
+    betas = np.arange(0.1, 1.0, 0.1)
+    coords = np.array([[1, i] for i in np.arange(0.1, 1.1, 0.1)])
+
+    cythonGPs = [george.GP(ExpSquaredKernel(beta, ndim=2L))
+                 for beta in betas]
+    cythonCov = [cythonGP.get_matrix(coords)
+                 for cythonGP in cythonGPs]
+
+    pythonCov = [plotExpSqCov(coords, beta=beta, plot2=False)
                  for beta in betas]
 
     for i in range(len(betas)):
